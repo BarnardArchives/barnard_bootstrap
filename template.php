efdfd74a904d798a5339b32eab5d7c9ec8ab2ad2
Benjamin Rosner <brosner@islandoradev.bcit.test>
2019-01-11 10:47:09 -0500

    Commit 1: Frontpage, scss
    Update templates and sass. Remove errant file in .sass-cache. Add .gitignore to exclude that dir.

--- template.php
+++ template.php
@@ -3,3 +3,26 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Implements hook_preprocess_islandora_basic_collection_wrapper().
+ */
+function bootstrap_preprocess_islandora_basic_collection_wrapper(&$vars) {
+  $object = $vars['islandora_object'];
+
+  if (isset($object['MODS']) && $mods = simplexml_load_string($object['MODS']->getContent(NULL))) {
+    $mods_local_identifier = (string) $mods->identifier;
+    $is_student_pub = $object->id == variable_get('bc_islandora_student_pubs_pid', 'islandora:1022');
+
+    if (empty($mods_local_identifier) && !$is_student_pub) {
+      return;
+    }
+
+    $is_record_group12 = preg_match("/BC(12)-(\d{2})/", $mods_local_identifier);
+
+    // If this is a BC12 object, $vars['student_pubs'] is TRUE.
+    if ($is_record_group12 || $is_student_pub) {
+      $vars['student_pubs'] = TRUE;
+    }
+  }
+}
--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -48,40 +48,7 @@
 </div>
 
 <?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
+  <footer class="front-footer <?php print $container_class; ?>">
     <?php print render($page['footer']); ?>
   </footer>
 <?php endif; ?>
-
-<!--
-<div id="page">
-
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
-
-  </div>
-
-  <?php print render($page['footer']); ?>
-
-</div>
-
-<?php print render($page['bottom']); ?>
--->
