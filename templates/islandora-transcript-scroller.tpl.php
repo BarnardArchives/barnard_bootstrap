cf7c15a3874036335ba8026e3e60c22c64458aa6
Benjamin Rosner <brosner@barnard.edu>
2019-09-10 11:33:27 -0400

    Merged branch.

--- templates/islandora-transcript-scroller.tpl.php
+++ templates/islandora-transcript-scroller.tpl.php
@@ -6,7 +6,7 @@
 	<h3>Transcript</h3>
   <div class="transcript-content" id="transcript">
     <?php if (empty($transcript['contents']['tcu_list'])): ?>
-      this transcript has not been corrected; you can download an automated, uncorrected transcript by clicking on the link above
+      This transcript has not been corrected; you may download an automated, uncorrected transcript by clicking on the link above.
     <?php else: ?>
       <?php print render($transcript); ?>
     <?php endif; ?>
