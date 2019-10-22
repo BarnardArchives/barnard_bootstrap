7d3831371c8a4b5eae18b6b76404e75416f27cb9
Benjamin Rosner <brosner@barnard.edu>
2019-10-22 13:55:43 -0400

    Pagination and Ellipsis update.

--- js/barnard_pagination.js
+++ js/barnard_pagination.js
@@ -8,29 +8,36 @@
   Drupal.behaviors.barnardIslandoraPagination = {
     attach: function (context, settings) {
       const islandoraPager = $('ul.pagination'), // Grab the pagination item and hold it.
-          activePage = islandoraPager.find('li.active'), // active position
+          pagesArray = islandoraPager.find('li').toArray().map( (i) => { return i.innerText }),
           firstPage = islandoraPager.find('li.pager-first > a'), // first item
           lastPage = islandoraPager.find('li.pager-last > a'), // last item
-          lastPageNumber = lastPage.length ? lastPage.attr('href').match(/.*page=(\d*)/i)[1] : 0; // greatest possible page number
+          lastPageNumber = lastPage.length ? Number(lastPage.attr('href').match(/.*page=(\d*)/i)[1]) + 1 : 0, // greatest possible page number
+          ellips = $('.pager-ellipsis.disabled');
       // lastPageNumber = $.urlParams(lastPage.attr('href')).page; // greatest possible page number - this requires a newer version of jQuery!
 
-      if (activePage.text() > 3) {
+      console.log(pagesArray);
+
+      if (pagesArray.includes("1")) {
+        firstPage.hide();
+      } else {
         firstPage.text('1'); // set the first page text to the number 1.
         firstPage.parent().insertAfter(islandoraPager.find('li.prev')); // move the item forward
-      } else {
-        firstPage.hide(); // otherwise we don't need it
       }
 
-      if (activePage.text() === '4') {
-        firstPage.parent().next().hide();
-      }
-
-      if (activePage.text() < (lastPageNumber - 1)) {
-        lastPage.text(Number(lastPageNumber) + 1); // set the last page to the last page number
-        lastPage.parent().insertBefore(islandoraPager.find('li.next')); // move the item backward
-      } else {
+      if (pagesArray.includes(String(lastPageNumber))) {
         lastPage.hide(); // otherwise we do not need it.
+      } else {
+        lastPage.text(lastPageNumber); // set the last page to the last page number
+        lastPage.parent().insertBefore(islandoraPager.find('li.next')); // move the item backward
       }
+
+      ellips.each( (e,t) => {
+        let ellipsis = $(t);
+        if (Number(ellipsis.prev().text()) + 1 === Number(ellipsis.next().text())) {
+          ellipsis.hide();
+        }
+      });
+      
     }
   };
 })( jQuery );
